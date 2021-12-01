fb66ab7895d5675744bc0a4202a4148d03843ac1
Filip Tuczyński <ftuczynski@gmail.com>
2021-12-01 21:09:36 +0100

    Add Day1

--- AdventD01/Program.cs
+++ AdventD01/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventD01
 {
@@ -10,6 +9,26 @@ namespace AdventD01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("------ Advent Of Code Day 1 ------");
+            var input = File.ReadAllLines("input.txt").Select(x => int.Parse(x)).ToList();
+
+            //PART 1
+            Console.WriteLine("Part1: " + Solve(input, 1));
+
+            //PART 2
+            Console.WriteLine("Part2: " + Solve(input, 3));
+        }
+
+        static int Solve(List<int> input, int windowSize)
+        {
+            var result = 0;
+            for (int i = 0; i < input.Count - windowSize; i++)
+            {
+                var sum1 = input.Skip(i).Take(windowSize).Sum();
+                var sum2 = input.Skip(i+1).Take(windowSize).Sum();
+                if (sum1 < sum2) result++;
+            }
+            return result;
         }
     }
 }
