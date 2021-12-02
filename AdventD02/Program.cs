1e143fad53e6612a039f3a7e3d27d4d1a3bd45bf
Filip Tuczyński <ftuczynski@gmail.com>
2021-12-02 19:34:46 +0100

    Fix Day2

--- AdventD02/Program.cs
+++ AdventD02/Program.cs
@@ -11,25 +11,30 @@ namespace AdventD02
             Console.WriteLine("------ Advent Of Code Day 2 ------");
 
             var input = File.ReadAllLines("input.txt").Select(x => new { direction = x.Split(' ')[0], value = int.Parse(x.Split(' ')[1]) }).ToList();
-            int horizontalPosition = 0;
-            int depth = 0;
-            int aim = 0;
+            int h = 0, d = 0, a = 0;
 
             foreach (var row in input)
             {
-                if (row.direction == "forward")
+                switch (row.direction)
                 {
-                    horizontalPosition += row.value;
-                    depth += row.value * aim;
+                    case "forward":
+                        h += row.value;
+                        d += row.value * a;
+                        break;
+                    case "down":
+                        a += row.value;
+                        break;
+                    case "up":
+                        a -= row.value;
+                        break;
                 }
-                if (row.direction == "down") aim += row.value;
-                if (row.direction == "up") aim -= row.value;
             }
+
             //PART 1
-            Console.WriteLine("Part1: " + horizontalPosition * aim);
+            Console.WriteLine("Part1: " + h * a);
 
             //PART 2
-            Console.WriteLine("Part2: " + horizontalPosition * depth);
+            Console.WriteLine("Part2: " + h * d);
         }
     }
 }
